03e7e8d64d984dc32762ac10e0ebe998502180f1
timerceo-lab <timerceo@gmail.com>
2025-09-28 22:12:25 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Database files
 const GLOBAL_USERS_FILE = 'globalUsers.json';
